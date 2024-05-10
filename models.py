8fd7af8794fd322e43b82f6eb7cb80c22cd9c580
hesam835 <hesamshadmehri@gmail.com>
2024-05-10 16:37:51 -0400

    create models, database

---
+++ models.py
@@ -0,0 +1,28 @@
+from database import Base
+from sqlalchemy import Column,String,Integer
+
+
+class Company(Base):
+    __tablename__ = "company"
+    
+    id = Column(Integer, primary_key=True)
+    sales_number = Column(Integer)
+    buy_number = Column(Integer)
+    sales_valume = Column(Integer)
+    buy_valume = Column(Integer)
+    sales = Column(Integer)
+    buy = Column(Integer)
+    
+    
+class FirstBoourseMarket(Base):
+    __tablensme__ = "Bourse"
+    
+    id = Column(Integer, primary_key=True)
+    last_transaction = Column(Integer)
+    final_price = Column(Integer)
+    first_price = Column(Integer)
+    yesterday_price = Column(Integer)
+    transactions_number = Column(Integer)
+    volume_transaction = Column(Integer)
+    transaction_value = Column(Integer)
+    market_value = Column(Integer)
